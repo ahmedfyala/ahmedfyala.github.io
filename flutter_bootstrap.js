246b3da6fa9b82ef56f9eefd6970872981e73040
ahmedfyala <afyala115@gmail.com>
2025-07-12 21:23:29 +0300

    fix fonts

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "207210474"
+    serviceWorkerVersion: "3166380096"
   }
 });
